6f96caf869e350041dc0dfdff4fb728f6e271f98
Erik Aybar <eaybar@practicegenius.com>
2014-09-16 07:45:27 -0600

    Routes/MembersController - Updates/Implements all Resourceful methods
    - Adds getMemberUsingSlug($name_slug)
    - TODO: Extract User/Member/Model creator ...
    - TODO: Extract User/Member/Model validation ...
    - TODO: Extract User/Member/Model sanitizer ...

--- app/controllers/MembersController.php
+++ app/controllers/MembersController.php
@@ -1,93 +1,149 @@
 <?php
 
-class MembersController extends BaseController {
+/**
+ * Class MembersController
+ * @author Erik Aybar
+ */
+class MembersController extends BaseController
+{
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /members
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return View::make('members.index');
-	}
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        $members = User::get();
+        $data    = compact('members');
+        return View::make('members.index', $data);
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /members/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-        $data = [
-            'member' => new User
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function create()
+    {
+        $member = new User;
+        $data   = compact('member');
+        return View::make('members.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @return Response
+     */
+    public function store()
+    {
+        $member_create_data = Input::only(['first_name', 'last_name', 'email', 'password', 'password_confirmation']);
+        $rules              = [
+            'first_name' => 'required',
+            'last_name'  => 'required',
+            'email'      => 'required|email|unique:users',
+            'password'   => 'required|min:6|alpha_num|confirmed'
         ];
-		return View::make('members.create', $data);
-	}
+        $validator          = Validator::make($member_create_data, $rules);
+        if (!$validator->passes()) {
+            return Redirect::back()
+                ->withInput()
+                ->withErrors($validator->errors())
+                ->withFlashMessage('Whoops. Try again!');
+        }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /members
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+        $member_create_data['password'] = Hash::make($member_create_data['password']);
+        $member_create_data['email']    = strtolower($member_create_data['email']);
+        $new_member                     = User::create($member_create_data);
 
-	/**
-	 * Display the specified resource.
-	 * GET /members/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-        $member = User::where('email', 'erikthedeveloper@gmail.com')->firstOrFail();
-        $data   = compact('member');
-		return View::make('members.show', $data);
-	}
+        return Redirect::route('members.show', $new_member->makeSlug());
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /members/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-        $member = User::where('email', 'erikthedeveloper@gmail.com')->firstOrFail();
-        $data   = compact('member');
-		return View::make('members.edit', $data);
-	}
+    /**
+     * Display the specified resource.
+     * @param  int $name_slug
+     * @return Response
+     */
+    public function show($name_slug)
+    {
+        try {
+            $member = $this->getMemberUsingSlug($name_slug);
+        } catch (Exception $e) {
+            return Redirect::route('members.index');
+        }
+        $data = compact('member');
+        return View::make('members.show', $data);
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /members/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     * @param $name_slug
+     * @return Response
+     */
+    public function edit($name_slug)
+    {
+        try {
+            $member = $this->getMemberUsingSlug($name_slug);
+        } catch (Exception $e) {
+            return Redirect::route('members.index');
+        }
+        $data = compact('member');
+        return View::make('members.edit', $data);
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /members/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * @param $name_slug
+     * @throws Exception
+     * @return Response
+     */
+    public function update($name_slug)
+    {
+        $member = $this->getMemberUsingSlug($name_slug);
 
+        $member_update_data = Input::only(['first_name', 'last_name', 'email']);
+        $rules              = ['first_name' => 'required', 'last_name' => 'required'];
+        $rules['email']     = ($member_update_data['email'] != $member->email) ? 'required|email|unique:users' : '';
+        $validator          = Validator::make($member_update_data, $rules);
+        if (!$validator->passes()) {
+            return Redirect::back()
+                ->withInput()
+                ->withErrors($validator->errors());
+        }
+
+        $member->update($member_update_data);
+        return Redirect::route('members.edit', $member->makeSlug());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param $name_slug
+     * @throws Exception
+     * @return Response
+     */
+    public function destroy($name_slug)
+    {
+        try {
+            $member = $this->getMemberUsingSlug($name_slug);
+        } catch (Exception $e) {
+            return Redirect::route('members.index');
+        }
+        $deleted = $member->delete();
+        return Redirect::route('members.index')
+            ->withFlashMessage('Member deleted...');
+    }
+
+    /**
+     * @param $first
+     * @param $last
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @return User
+     * @author Erik Aybar
+     */
+    public function getMemberUsingSlug($name_slug)
+    {
+        $name_segments = explode('-', $name_slug);
+        $first         = $name_segments[0];
+        $last          = $name_segments[1];
+        $member        = User::where('first_name', $first)->where('last_name', $last)->firstOrFail();
+        return $member;
+    }
 }
--- app/routes.php
+++ app/routes.php
@@ -5,11 +5,11 @@ Route::get('our-mission', ['as' => 'mission',       'uses' => 'GenericPagesContr
 
 
 // Route::resource('members', 'MembersController');
-Route::get(   'members',               ['as' => 'members.index',   'uses' => 'MembersController@index']);
-Route::get(   'sign-up',               ['as' => 'members.create',  'uses' => 'MembersController@create']);
-Route::post(  'members',               ['as' => 'members.store',   'uses' => 'MembersController@store']);
-Route::get(   'members/{member_name}', ['as' => 'members.show',    'uses' => 'MembersController@show']);
-Route::get(   'members/{member_name}/edit', ['as' => 'members.edit',    'uses' => 'MembersController@edit']);
-Route::get(   'my-profile',            ['as' => 'my.profile',    'uses' => 'MembersController@edit']);
-Route::put(   'members/{member_name}', ['as' => 'members.update',  'uses' => 'MembersController@update']);
-Route::delete('members/{member_name}', ['as' => 'members.destroy', 'uses' => 'MembersController@destroy']);
+Route::get(   'members',                  ['as' => 'members.index',   'uses' => 'MembersController@index']);
+Route::get(   'sign-up',                  ['as' => 'members.create',  'uses' => 'MembersController@create']);
+Route::post(  'members',                  ['as' => 'members.store',   'uses' => 'MembersController@store']);
+Route::get(   'members/{name_slug}',      ['as' => 'members.show',    'uses' => 'MembersController@show']);
+Route::get(   'members/{name_slug}/edit', ['as' => 'members.edit',    'uses' => 'MembersController@edit']);
+Route::get(   'my-profile',               ['as' => 'my.profile',      'uses' => 'MembersController@edit']);
+Route::put(   'members/{name_slug}',      ['as' => 'members.update',  'uses' => 'MembersController@update']);
+Route::delete('members/{name_slug}',      ['as' => 'members.destroy', 'uses' => 'MembersController@destroy']);
